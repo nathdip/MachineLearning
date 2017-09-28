5b42f042701b84443a30b1e88f54c575cbd0442f
dnath <nathdip@gmail.com>
2017-09-28 18:24:48 +0200

    minor changes

--- CostFunctionandGradient.py
+++ CostFunctionandGradient.py
@@ -5,6 +5,9 @@ Created on Tue Sep 26 13:14:49 2017
 
 @author: atlas
 """
+
+from numpy import*
+
 def sigmoid(z):
     
     return 1/(1+exp(-z))
@@ -12,6 +15,7 @@ def sigmoid(z):
     
 def costandGradient(X,y,theta,lregularize):
     
+    y=matrix.transpose(array([y]))
     m=len(y)
     z=dot(X,theta)
     cost=-y*sigmoid(z)-(1-y)
